4409f8fc01ed138149152bd5ead5a429b2083bca
Mariusz Pasek <pasiasty@gmail.com>
2022-06-15 14:02:04 +0200

    fixing session view unittests

--- ui/src/app/session-view/session-view.component.spec.ts
+++ ui/src/app/session-view/session-view.component.spec.ts
@@ -8,12 +8,13 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopBarComponent } from 'src/app/top-bar/top-bar.component';
 import { Component, Renderer2 } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { ToastsContainerComponent } from 'src/app/toasts-container/toasts-container.component';
 import { MonacoEditorService } from 'src/app/monaco-editor/monaco-editor.service';
 import { MonacoEditorComponent } from 'src/app/monaco-editor/monaco-editor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiService, EditResponse, GetSessionResponse, User } from '../services/api.service';
 
 @Component({
   template: `
@@ -28,6 +29,18 @@ class TestHostComponent {
 describe('SessionViewComponent', () => {
   let component: TestHostComponent;
   let fixture: ComponentFixture<TestHostComponent>;
+  let sessionObservable: Observable<EditResponse>;
+  let monacoEditorService: MonacoEditorService;
+  let renderer2: Renderer2;
+
+  const apiServiceSpy = jasmine.createSpyObj('ApiService', [
+    'GetSession',
+    'StartSession',
+    'GetUserID',
+    'UpdateSession',
+    'SessionObservable',
+    'openLSPWebsocket',
+  ]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -53,6 +66,10 @@ describe('SessionViewComponent', () => {
           useValue: {
             params: of({ session_id: "abcdefabcdef" }),
           },
+        },
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
         }
       ],
     })
@@ -60,13 +77,34 @@ describe('SessionViewComponent', () => {
   });
 
   beforeEach(() => {
+    sessionObservable = new Observable<EditResponse>();
+
+    apiServiceSpy.GetSession.and.returnValue(new Promise<GetSessionResponse>((resolve, reject) => {
+      resolve({ Language: "python", Text: "aaa", InputText: "", Stdout: "", Stderr: "" });
+    }));
+    apiServiceSpy.SessionObservable.and.returnValue(sessionObservable);
+    apiServiceSpy.GetUserID.and.returnValue('user-id');
+    apiServiceSpy.UpdateSession.and.callFake((baseText: string, newText: string, cursorPos: number, otherUsers: User[], selection?: Selection) => {
+      console.log('Called UpdateSession');
+    });
+
     fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    monacoEditorService = fixture.debugElement.injector.get(MonacoEditorService);
+    renderer2 = fixture.debugElement.injector.get(Renderer2);
+
+    renderer2.removeClass(document.body, 'bootstrap');
+    renderer2.addClass(document.body, 'bootstrap-dark');
+
+    monacoEditorService.load();
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy();
+
+    // workaround until session view correctly reports that all editors were created properly.
+    await new Promise(f => setTimeout(f, 1000));
   });
 });
 

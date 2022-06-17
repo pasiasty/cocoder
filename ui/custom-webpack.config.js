de82a6027552bfe1c8bedc1af098c2b2fc7fe27d
Mariusz Pasek <pasiasty@gmail.com>
2022-06-17 10:40:01 +0200

    removing unused alias

--- ui/custom-webpack.config.js
+++ ui/custom-webpack.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   "resolve": {
-  //   "alias": {
-  //     "vscode": require.resolve("./node_modules/monaco-languageclient/lib/vscode-compatibility")
-  //   },
     "fallback": {
       "path": require.resolve("path-browserify")
     }

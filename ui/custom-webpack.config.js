ae181f9c44ce6663436e65154a5e26f9c70fbf85
Mariusz Pasek <pasiasty@gmail.com>
2022-06-17 12:38:48 +0200

    downgrading monaco-languageclient to non-dev version

--- ui/custom-webpack.config.js
+++ ui/custom-webpack.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   "resolve": {
+    "alias": {
+      "vscode": require.resolve("./node_modules/monaco-languageclient/lib/vscode-compatibility")
+    },
     "fallback": {
       "path": require.resolve("path-browserify")
     }
--- ui/src/app/monaco-editor/monaco-editor.service.ts
+++ ui/src/app/monaco-editor/monaco-editor.service.ts
@@ -6,6 +6,8 @@ import { toSocket, WebSocketMessageReader, WebSocketMessageWriter } from '@codin
 
 import { ApiService } from '../services/api.service';
 
+import * as monaco from 'monaco-editor';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -57,7 +59,7 @@ export class MonacoEditorService {
 
   private registerLanguages() {
     // install Monaco language client services
-    MonacoServices.install();
+    MonacoServices.install(monaco);
 
     // create the web socket
     this.apiService.openLSPWebsocket('python', this.languageOnOpenHandler('Python', ['python']));
